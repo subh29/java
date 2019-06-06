5b21e2c4656a466f964bf2251d383dbb4f1c7cfa
Subh <subhrendusahu20@gmail.com>
2019-06-06 14:32:09 +0530

    print Hello 10 times

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,11 @@
 public class HelloWorld {
    public static void main(String[] args) {
+	   int i=0;
+	   int n=10;
+	   for(i=0;i<=n;i++)
+	   {
       // Prints "Hello, I am here in the terminal window.
       System.out.println("Hello all, Welcome to Jenkins and DevOps");
+	   }
    }
 }
